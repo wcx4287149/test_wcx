c1fc3ea4ba7c67b1e9f76f8f410f1329d186fd48
wangcx <wangcx@szlhxx.com>
2021-04-25 22:53:01 +0800

    1

---
+++ app/test.php
@@ -0,0 +1,7 @@
+<?php
+ namespace App;
+ class test{
+    public function test(){
+        echo mt_rand(10000,99999);
+    }
+ }
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    require "./vendor/autoload.php";
+    $test=new App\test();
+    $test->test();
